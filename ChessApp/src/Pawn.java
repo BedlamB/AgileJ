29f5eaddbf0e03c847cfd0d60c2a92fabcd8e359
Vicky Stevens <bedlam@gmail.com>
2014-08-27 15:43:27 +0100

    added chess stuff

---
+++ ChessApp/src/Pawn.java
@@ -0,0 +1,15 @@
+/**
+ * Created by vickys on 27/08/14.
+ */
+public class Pawn {
+
+    private String colour;
+
+    public Pawn(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
